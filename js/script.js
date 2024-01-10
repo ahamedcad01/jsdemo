f4d6ac35966206d0f086b3b5b2dc947a92e7dbeb
ahamedcad01 <ahamedcad01@gmail.com>
2024-01-10 15:56:23 +0530

    files updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
-function findGrade1(m,k){
+ console.log("welcome to js");
 
-var mark= (35<=m && m<60) ? "grade c" : (60<=m && m<80) ? "Grade B" : (80<=m && m<90) ? "Grade A" : (90<=m && m<=100) ? "Grade Distinction" : "Fail";
-console.log(k,mark); // output
+var a = 10;
+let c = 20;
 
+const d = 3.14;
+
+
+a = true;
+console.log(typeof a);
+
+var b = parseInt(a);
+console.log(typeof b);
+
+
+var a = "100";
+console.log(typeof a);
+
+a  = parseInt(a);
+console.log(a,typeof a);
+
+var n = a.toString();
+console.log(n,typeof n);
+
+var v = 10;
+console.log(v++);
+var v1 = 20;
+console.log(v);
+var a = 10;
+var m = a << 2;
+console.log(m);
+
+
+function checkvalue(a){
+    console.log(a);
+    var a=Number(a);
+switch(a){
+
+    case 1:
+        console.log("formal collections");
+        break ;
+    case 2:
+        console.log("casual collections");
+        break ;
+    default:
+        console.log("main menu");
+        break ;
 
 }
-
-findGrade1(90,"ahamed");
+}
+checkvalue(prompt("enter value"));
